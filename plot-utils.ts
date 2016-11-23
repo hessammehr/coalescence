efebbfdbbdfcda50d1b9e3d65be3fac5bc8b3280
Hessam Mehr <hessammehr@gmail.com>
2016-11-23 01:10:16 -0800

    Updated to d3 v4 API.

--- plot-utils.ts
+++ plot-utils.ts
@@ -1,51 +1,50 @@
-import d3 from "d3";
+import * as d3 from "d3";
 
-function* axes(xtitle, ytitle, changeLimits) {
+export function* axes(xtitle, ytitle, changeLimits) {
 
-  var margin = {top: 20, right: 25, bottom: 30, left: 50},
-  width = 480 - margin.left - margin.right,
-  height = 180 - margin.top - margin.bottom;
+  let margin = { top: 20, right: 25, bottom: 30, left: 50 },
+    width = 480 - margin.left - margin.right,
+    height = 180 - margin.top - margin.bottom;
 
-  var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  var colors = d3.scale.category10();
-  var x = d3.scale.linear().range([0, width]);
-  var y = d3.scale.linear().range([height, 0]);
-  var x_axis = d3.svg.axis().scale(x).orient("bottom")
-  var y_axis = d3.svg.axis().scale(y).orient("left");
-  var x_node = svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")");
-  var y_node =svg.append("g")
-  .attr("class", "y axis");
+  let svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+  let colors = d3.scaleOrdinal(d3.schemeCategory10);
+  let x = d3.scaleLinear().range([0, width]);
+  let y = d3.scaleLinear().range([height, 0]);
+  let x_axis = d3.axisBottom(x);
+  let y_axis = d3.axisLeft(y);
+  let x_node = svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")");
+  let y_node = svg.append("g")
+    .attr("class", "y axis");
 
   x_node.append("text")
-  .attr("transform", "translate("+ width + ", 0)")
-  .text(xtitle);
+    .attr("transform", "translate(" + width + ", 0)")
+    .text(xtitle);
   y_node.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 6)
-  .attr("dy", ".71em")
-  .style("text-anchor", "end")
-  .text(ytitle);
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text(ytitle);
 
-  var l = d3.svg.line()
-  .x(function(d,n) { return x(n); })
-  .y(function(d) { return y(d); })
-  .interpolate("monotone");
-  var p = svg.append("path")
-  .attr("class", "line")
-  .attr("stroke", colors(1));
+  let l = d3.line()
+    .x(function (d, n) { return x(n) })
+    .y(function (d) { return y(d[1]) });
+  let p = svg.append("path")
+    .attr("class", "line")
+    .attr("stroke", colors("1"));
 
-  var first = true;
-  while(true) {
-    data = yield svg;
-    var ymin = Math.min.apply(null, data);
-    var ymax = Math.max.apply(null, data);
+  let first = true;
+  while (true) {
+    let data = yield svg;
+    let ymin = Math.min(...data);
+    let ymax = Math.max(...data);
     if (changeLimits || first) {
       first = false;
       x.domain([0, data.length]);
@@ -56,12 +55,12 @@ function* axes(xtitle, ytitle, changeLimits) {
     y_node.call(y_axis);
 
     p.datum(data)
-    .attr("d", l);
+      .attr("d", l);
   }
 }
 
-function monitor(g, a, i) {
-  setInterval(function() {
+export function monitor(g, a, i) {
+  setInterval(function () {
     a.next(g.next().value);
   }, i)
 }
